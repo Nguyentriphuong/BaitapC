872df7c85a5a6f11942cff31530793664a46714e
Nguyentriphuong <Nguyentriphuong2107@gmail.com>
2018-12-24 05:03:28 +0700

    Initial commit

---
+++ Context/CustomerContext.cs
@@ -0,0 +1,14 @@
+﻿using Baitap.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Baitap.Context
+{
+    public class CustomerContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
